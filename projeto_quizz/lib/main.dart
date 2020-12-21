db5d6175db198d67c6fd5cd22296e95a5640bc3e
derek-alves <derek.alves1998@hotmail.com>
2020-12-21 08:03:44 -0300

    Components

--- projeto_quizz/lib/main.dart
+++ projeto_quizz/lib/main.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import './questão.dart';
-import './resposta.dart';
+import './questionario.dart';
+import './resultado.dart';
 
 main() => runApp(QuizzApp());
 
 class _PerguntaAppState extends State<QuizzApp> {
   var _perguntaSelecionada = 0;
 
+  final List<Map<String, Object>> _perguntas = const [
+    {
+      "texto": "Qual é sua cor favorita?",
+      "respostas": ['Preto', 'Vermelho', 'Verde', 'Branco']
+    },
+    {
+      "texto": "Qual é o seu animal favorito?",
+      "respostas": ['Cavalo', 'Carneiro', 'Tatu', 'Vaca']
+    },
+    {
+      "texto": "Qual é o seu instrutor favorito?",
+      "respostas": ['Maria', 'Paulo', 'Pablo', 'Parang']
+    }
+  ];
+
   void _responder() {
-    setState(() {
-      _perguntaSelecionada++;
-    });
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   void Function() funcaoQueRetornaOutraFuncao() {
@@ -22,63 +43,18 @@ class _PerguntaAppState extends State<QuizzApp> {
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, Object>> perguntas = [
-      {
-        "texto": "Qual é sua cor favorita?",
-        "respostas": ['Preto', 'Vermelho', 'Verde', 'Branco']
-      },
-      {
-        "texto": "Qual é o seu animal favorito?",
-        "respostas": ['Cavalo', 'Carneiro', 'Tatu', 'Vaca']
-      },
-      {
-        "texto": "Qual é o seu instrutor favorito?",
-        "respostas": ['Maria', 'Paulo', 'Pablo', 'Parang']
-      }
-    ];
-
-    List<Widget> respostas = [];
-
-    for (String textoResp in perguntas[_perguntaSelecionada]['respostas']) {
-      respostas.add(Resposta(textoResp, _responder));
-    }
-
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
           title: Text('Quizz App'),
         ),
-        body: Column(
-          children: <Widget>[
-            Questao(perguntas[_perguntaSelecionada]['texto']),
-            ...respostas,
-            // //Exemplos
-            //
-            // RaisedButton(
-            //   child: Text('Resposta 1'),
-            //   onPressed: _responder,
-            // ),
-            // RaisedButton(
-            //   child: Text('exemplo 2'),
-            //   onPressed: funcaoQueRetornaOutraFuncao(),
-            // ),
-            // RaisedButton(
-            //   child: Text('exemplo 3'),
-            //   onPressed: () {
-            //     print('Resposta numero 3');
-            //   },
-            // ),
-            // RaisedButton(
-            //   child: Text('exemplo 4'),
-            //   onPressed: () => print('Resposta numero 4'),
-            // ),
-            // Column(
-            //   children: [],
-            // )
-          ],
-        ),
+        body: temPerguntaSelecionada
+            ? Questionario(
+                perguntas: _perguntas,
+                perguntaSelecionada: _perguntaSelecionada,
+                responder: _responder)
+            : Resultado(),
       ),
     );
   }
