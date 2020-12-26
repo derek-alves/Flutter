5a1e0f59731ea90ed0f1c514a2a2cd72881f5959
derek-alves <derek.alves1998@hotmail.com>
2020-12-26 19:15:24 -0300

    Components

--- despesas/lib/main.dart
+++ despesas/lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:despesas/components/transaction_form.dart';
+import 'package:despesas/components/transaction_list.dart';
 import 'package:despesas/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -15,9 +16,6 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
   final _transactions = [
     Transaction(
       id: 't1',
@@ -56,79 +54,8 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Column(
-            children: _transactions.map((tr) {
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      width: 100,
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10,
-                      ),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      )),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        'R\$ ${tr.value.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          tr.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          DateFormat('d MMM y').format(tr.data),
-                          style: TextStyle(
-                            color: Colors.grey[600],
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
-          Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  TextField(
-                    controller: titleController,
-                    decoration: InputDecoration(labelText: 'Título'),
-                  ),
-                  TextField(
-                    controller: valueController,
-                    decoration: InputDecoration(labelText: 'Valor(R\$)'),
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      print(titleController.text);
-                    },
-                    textColor: Colors.purple,
-                    child: Text('Nova Transação'),
-                  )
-                ],
-              ),
-            ),
-          )
+          TransactionList(_transactions),
+          TransactionForm(),
         ],
       ),
     );
