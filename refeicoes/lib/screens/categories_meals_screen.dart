d01cb5774dfe37367ed9b69547408993fac547a1
derek-alves <derek.alves1998@hotmail.com>
2021-01-12 12:14:43 -0300

    Meals card

--- refeicoes/lib/screens/categories_meals_screen.dart
+++ refeicoes/lib/screens/categories_meals_screen.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:refeicoes/models/meal.dart';
 import '../models/category.dart';
+import '../data/dummy_data.dart';
+import '../components/meal_item.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
     final category = ModalRoute.of(context).settings.arguments as Category;
+
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: Center(
-        child: Text('Receitas por categoria'),
+      body: NewWidget(categoryMeals: categoryMeals),
+    );
+  }
+}
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    Key key,
+    @required this.categoryMeals,
+  }) : super(key: key);
+
+  final List<Meal> categoryMeals;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index) {
+          return MealItem(categoryMeals[index]);
+        },
       ),
     );
   }
