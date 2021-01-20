3715a2676b67bfbd589a939fa6ff1440ebff89ee
derek-alves <derek.alves1998@hotmail.com>
2021-01-20 00:22:30 -0300

    Navegacao com pop

--- refeicoes/lib/screens/meal_detail_screen.dart
+++ refeicoes/lib/screens/meal_detail_screen.dart
@@ -32,55 +32,62 @@ class MealDetailScreen extends StatelessWidget {
     final meal = ModalRoute.of(context).settings.arguments as Meal;
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text(meal.title),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 200,
-                width: double.infinity,
-                child: Image.network(
-                  meal.imageUrl,
-                  fit: BoxFit.cover,
-                ),
+      appBar: AppBar(
+        title: Text(meal.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
               ),
-              _createSectionTitle(context, "Ingredientes"),
-              _createSectionContainer(
-                ListView.builder(
-                  itemCount: meal.ingredients.length,
-                  itemBuilder: (ctx, index) {
-                    return Card(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 5, horizontal: 10),
-                        child: Text(meal.ingredients[index]),
-                      ),
-                      color: Theme.of(context).accentColor,
-                    );
-                  },
-                ),
-              ),
-              _createSectionTitle(context, "Passos"),
-              _createSectionContainer(ListView.builder(
-                itemCount: meal.steps.length,
+            ),
+            _createSectionTitle(context, "Ingredientes"),
+            _createSectionContainer(
+              ListView.builder(
+                itemCount: meal.ingredients.length,
                 itemBuilder: (ctx, index) {
-                  return Column(
-                    children: [
-                      ListTile(
-                        leading: CircleAvatar(
-                          child: Text('${index + 1}'),
-                        ),
-                        title: Text(meal.steps[index]),
-                      ),
-                      Divider(),
-                    ],
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 10),
+                      child: Text(meal.ingredients[index]),
+                    ),
+                    color: Theme.of(context).accentColor,
                   );
                 },
-              )),
-            ],
-          ),
-        ));
+              ),
+            ),
+            _createSectionTitle(context, "Passos"),
+            _createSectionContainer(ListView.builder(
+              itemCount: meal.steps.length,
+              itemBuilder: (ctx, index) {
+                return Column(
+                  children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        child: Text('${index + 1}'),
+                      ),
+                      title: Text(meal.steps[index]),
+                    ),
+                    Divider(),
+                  ],
+                );
+              },
+            )),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.star),
+        onPressed: () {
+          Navigator.of(context).pop(meal);
+        },
+      ),
+    );
   }
 }
