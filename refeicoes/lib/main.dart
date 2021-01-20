5b0a016c3832d56ebdc7fae84d8b1059e26259ab
derek-alves <derek.alves1998@hotmail.com>
2021-01-19 23:52:43 -0300

    Drawer navigation

--- refeicoes/lib/main.dart
+++ refeicoes/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:refeicoes/screens/meal_detail_screen.dart';
 import './screens/categories_screen.dart';
 import './screens/categories_meals_screen.dart';
 import './screens/tabs_screen.dart';
+import './screens/settings_screen.dart';
 import './utils/app_routes.dart';
 
 void main() => runApp(MyApp());
@@ -26,7 +27,8 @@ class MyApp extends StatelessWidget {
       routes: {
         AppRoutes.HOME: (ctx) => TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
-        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen()
+        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
+        AppRoutes.SETTINGS: (ctx) => SettingsScreen(),
       },
       onUnknownRoute: (settings) {
         return MaterialPageRoute(
