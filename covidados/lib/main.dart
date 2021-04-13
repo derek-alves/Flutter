8aa9e599ecb227cb40ea390e7e8417e6cbaaee48
derek-alves <derek.alves1998@hotmail.com>
2021-04-13 11:22:37 -0300

    landing page responsive

--- covidados/lib/main.dart
+++ covidados/lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:covidados/src/presentation/landingPage/landing_page.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => MyApp(), // Wrap your app
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
--- covidados/lib/src/presentation/landingPage/landing_page.dart
+++ covidados/lib/src/presentation/landingPage/landing_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -17,13 +18,13 @@ class LandingPage extends StatelessWidget {
                 style: TextStyle(
                   color: Colors.white,
                   fontWeight: FontWeight.w300,
-                  fontSize: 22,
+                  fontSize: screenHeight / 28,
                 ),
               ),
               Text(
                 "CORONAVIRUS",
                 style: TextStyle(
-                  fontSize: 28,
+                  fontSize: screenHeight / 24,
                   fontWeight: FontWeight.bold,
                 ),
               ),
@@ -32,63 +33,87 @@ class LandingPage extends StatelessWidget {
         ),
       ),
       backgroundColor: Theme.of(context).accentColor,
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        alignment: Alignment.center,
-        child: Column(
-          children: [
-            Container(
-              width: 270,
-              child: Text(
-                "Fique atento aos dados atualizados de diferentes países",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Image(
-              height: 300,
-              image: AssetImage("assets/image-1.png"),
-            ),
-            Container(
-                width: 270,
+      body: LayoutBuilder(builder: (ctx, constraints) {
+        return Container(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.fromLTRB(
+                  0,
+                  constraints.maxHeight * 0.01,
+                  0,
+                  constraints.maxHeight * 0.01,
+                ),
+                width: double.infinity,
                 child: Text(
-                  "Descubra como se proteger contra a",
+                  "Fique atento aos dados atualizados de diferentes países",
                   textAlign: TextAlign.center,
                   style: TextStyle(
-                      fontSize: 16,
                       color: Colors.white,
-                      fontWeight: FontWeight.w500),
-                )),
-            Text(
-              "Covid-19",
-              style: TextStyle(
-                  fontSize: 40,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 5),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                "Começar Agora",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+                      fontSize: screenHeight / 40,
+                      fontWeight: FontWeight.w400),
+                ),
               ),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 40),
-                primary: Color(0xff47A98C),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18)),
+              Expanded(
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Image(
+                    image: AssetImage("assets/image-1.png"),
+                    fit: BoxFit.contain,
+                  ),
+                ),
               ),
-            ),
-          ],
-        ),
-      ),
+              SizedBox(
+                height: 10,
+              ),
+              Column(
+                children: [
+                  Container(
+                    child: Text(
+                      "Descubra como se proteger contra a",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: constraints.maxHeight * 0.03,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  Text(
+                    "Covid-19",
+                    style: TextStyle(
+                        fontSize: constraints.maxHeight * 0.05,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: constraints.maxHeight * 0.03),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Começar Agora",
+                      style: TextStyle(
+                          fontSize: constraints.maxHeight * 0.03,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                      primary: Color(0xff47A98C),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18)),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: constraints.maxHeight * 0.09,
+              ),
+            ],
+          ),
+        );
+      }),
     );
   }
 }
