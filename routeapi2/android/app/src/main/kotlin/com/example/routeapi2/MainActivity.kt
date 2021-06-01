5cae7a612888d54bdefcd31290428c585ca347c1
derek-alves <derek.alves1998@hotmail.com>
2021-06-01 12:12:51 -0300

    first commit

---
+++ routeapi2/android/app/src/main/kotlin/com/example/routeapi2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.routeapi2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ routeapi2/lib/main.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BooksApp());
+}
+
+class Book {
+  final String title;
+  final String author;
+
+  Book(this.title, this.author);
+}
+
+class BooksApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _BooksAppState();
+}
+
+class _BooksAppState extends State<BooksApp> {
+  late Book? _selectedBook;
+  bool show404 = false;
+  List<Book> books = [
+    Book('Left Hand of Darkness', 'Ursula K. Le Guin'),
+    Book('Too Like the Lightning', 'Ada Palmer'),
+    Book('Kindred', 'Octavia E. Butler'),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _handleBookTapped(Book book) {
+    setState(() {
+      _selectedBook = book;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Books App',
+      home: Navigator(
+        pages: [
+          MaterialPage(
+            key: const ValueKey('BooksListPage'),
+            child: BooksListScreen(
+              books: books,
+              onTapped: _handleBookTapped,
+            ),
+          ),
+          if (show404)
+            MaterialPage(
+                key: const ValueKey('UnknownPage'), child: UnknownScreen())
+          else if (_selectedBook != null)
+            MaterialPage(
+                key: ValueKey(_selectedBook),
+                child: BookDetailsScreen(book: _selectedBook))
+        ],
+        onPopPage: (route, result) {
+          if (!route.didPop(result)) {
+            return false;
+          }
+
+          setState(() {
+            _selectedBook = null;
+          });
+
+          return true;
+        },
+      ),
+    );
+  }
+}
+
+class UnknownScreen {}
+
+class BookDetailsScreen extends StatelessWidget {
+  final Book? book;
+
+  const BookDetailsScreen({
+    Key? key,
+    required this.book,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (book != null) ...[
+              Text(book.title, style: Theme.of(context).textTheme.headline6),
+              Text(book.author, style: Theme.of(context).textTheme.subtitle1),
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BookDetailsPage extends Page {
+  final Book book;
+
+  BookDetailsPage({
+    required this.book,
+  }) : super(key: ValueKey(book));
+
+  @override
+  Route createRoute(BuildContext context) {
+    return PageRouteBuilder(
+      settings: this,
+      pageBuilder: (context, animation, animation2) {
+        final tween = Tween(begin: const Offset(0.0, 1.0), end: Offset.zero);
+        final curveTween = CurveTween(curve: Curves.easeInOut);
+        return SlideTransition(
+          position: animation.drive(curveTween).drive(tween),
+          child: BookDetailsScreen(
+            key: ValueKey(book),
+            book: book,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class BooksListScreen extends StatelessWidget {
+  final List<Book> books;
+  final ValueChanged<Book> onTapped;
+
+  const BooksListScreen({
+    Key? key,
+    required this.books,
+    required this.onTapped,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        children: [
+          for (var book in books)
+            ListTile(
+              title: Text(book.title),
+              subtitle: Text(book.author),
+              onTap: () => onTapped(book),
+            )
+        ],
+      ),
+    );
+  }
+}
