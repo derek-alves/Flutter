b4467e68906ba0df01f35fdf6902101cf138dd29
Derek Enrique Alves <derek.alves@usemobile.xyz>
2022-02-23 08:56:39 -0300

    🧩: create firebase messagin configuration and local notification to show

--- push_imp/lib/firebase_messagin/custom_firebase_messaging.dart
+++ push_imp/lib/firebase_messagin/custom_firebase_messaging.dart
@@ -1,1 +1,34 @@
-class CustomFirebaseMessaging {}
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+import 'package:push_imp/firebase_messagin/custom_local_notification.dart';
+
+class CustomFirebaseMessaging {
+  final CustomLocalNotification _customLocalNotification;
+
+  CustomFirebaseMessaging._internal(this._customLocalNotification);
+  static final CustomFirebaseMessaging _singleton = CustomFirebaseMessaging._internal(
+    CustomLocalNotification(),
+  );
+
+  factory CustomFirebaseMessaging() => _singleton;
+
+  Future<void> inicialize() async {
+    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(badge: true, sound: true);
+
+    FirebaseMessaging.onMessage.listen(
+      (message) {
+        RemoteNotification? notification = message.notification;
+        AndroidNotification? android = message.notification?.android;
+        AppleNotification? ios = message.notification?.apple;
+
+        if (notification != null && android != null) {
+          _customLocalNotification.androidNotification(notification, android);
+        } else if (notification != null && ios != null) {
+          _customLocalNotification.iosNotification(notification, ios);
+        }
+      },
+    );
+  }
+
+  getTokenFirebase() async => debugPrint(await FirebaseMessaging.instance.getToken());
+}
--- push_imp/lib/firebase_messagin/custom_local_notification.dart
+++ push_imp/lib/firebase_messagin/custom_local_notification.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class CustomLocalNotification {
@@ -10,6 +11,67 @@ class CustomLocalNotification {
       'High Importance Notifications',
       description: 'This channel is used for important notifications',
       importance: Importance.max,
+    );
+
+    _configuraAndroid().then((value) {
+      flutterLocalNotificationsPlugin = value;
+      inicializeNotifications();
+    });
+  }
+
+  Future<FlutterLocalNotificationsPlugin> _configuraAndroid() async {
+    var flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
+    return flutterLocalNotificationsPlugin;
+  }
+
+  androidNotification(
+    RemoteNotification notification,
+    AndroidNotification android,
+  ) {
+    flutterLocalNotificationsPlugin.show(
+      notification.hashCode,
+      notification.title,
+      notification.body,
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          channel.id,
+          channel.name,
+          channelDescription: channel.description,
+          icon: android.smallIcon,
+          priority: Priority.high,
+          styleInformation: BigTextStyleInformation(notification.body as String),
+        ),
+      ),
+    );
+  }
+
+  iosNotification(
+    RemoteNotification notification,
+    AppleNotification ios,
+  ) {
+    flutterLocalNotificationsPlugin.show(
+      notification.hashCode,
+      notification.title,
+      notification.body,
+      const NotificationDetails(
+        iOS: IOSNotificationDetails(),
+      ),
+    );
+  }
+
+  inicializeNotifications() {
+    const android = AndroidInitializationSettings('@mipmap/ic_launcher');
+    const ios = IOSInitializationSettings();
+
+    flutterLocalNotificationsPlugin.initialize(
+      const InitializationSettings(
+        android: android,
+        iOS: ios,
+      ),
     );
   }
 }
