7b3d53a260d7cfe41e659f1ddc550b7984e98687
derek-alves <derek.alves1998@hotmail.com>
2023-01-13 19:53:58 -0300

    step: add validation for the two events

--- frog_websocket/websocket_server/routes/ws.dart
+++ frog_websocket/websocket_server/routes/ws.dart
@@ -1,18 +1,38 @@
 import 'package:dart_frog/dart_frog.dart';
 import 'package:dart_frog_web_socket/dart_frog_web_socket.dart';
 import 'package:websocket_server/counter_cubit.dart';
+import 'package:websocket_server/message.dart';
 
 Future<Response> onRequest(RequestContext context) async {
   final handler = webSocketHandler((channel, protocol) {
     final cubit = context.read<CounterCubit>()..subscribe(channel);
-
     channel.sink.add('${cubit.state}');
     channel.stream.listen(
       (event) {
-        cubit.increment();
+        switch ('$event'.toMessage()) {
+          case Message.increment:
+            cubit.increment();
+            break;
+          case Message.decrement:
+            cubit.decrement();
+            break;
+          case null:
+            break;
+        }
       },
-      onDone: () => cubit.subscribe(channel),
+      onDone: () => cubit.unsubscribe(channel),
     );
   });
   return handler(context);
+}
+
+extension on String {
+  Message? toMessage() {
+    for (final message in Message.values) {
+      if (this == message.value) {
+        return message;
+      }
+    }
+    return null;
+  }
 }
